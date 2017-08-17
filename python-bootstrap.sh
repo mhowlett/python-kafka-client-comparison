20a6e91283560306e8e7fa94bdea29a493d7ab7c
Matt Howlett <matt@confluent.io>
2017-08-17 09:53:50 -0700

    update python bootstrap

--- python-bootstrap.sh
+++ python-bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cd /tmp
-wget -qO - http://packages.confluent.io/deb/3.3/archive.key | sudo apt-key add -
+wget -qO - http://packages.confluent.io/deb/3.3/archive.key | apt-key add -
 add-apt-repository "deb [arch=amd64] http://packages.confluent.io/deb/3.3 stable main"
 apt-get update
 apt-get install -y librdkafka-dev
+
+apt-get install -y emacs
+apt-get install -y less
+apt-get install -y libsnappy-dev
 
 # cd /tmp
 # git clone https://github.com/edenhill/librdkafka.git
@@ -18,11 +22,6 @@ apt-get install -y librdkafka-dev
 # pip install kafka-python
 pip install confluent-kafka
 # pip install pykafka
-
-apt-get update
-apt-get install -y emacs
-apt-get install -y less
-apt-get install -y libsnappy-dev
 
 cd /src
 bash
